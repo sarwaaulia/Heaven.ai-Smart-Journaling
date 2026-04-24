9c8fe40dd9ffe543ae36ec80911129169bfd9cf4
sarwa aulia <sarwaaulia01@example.com>
2026-04-24 19:02:24 +0700

    fixed success comments

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -33,7 +33,7 @@ export default function Register() {
 				return;
 			}
 
-			toast.success("Account created successfully! Please login.");
+			toast.success("Account created successfully!");
 			router.push("/auth/login");
 		} catch {
 			toast.error("Failed to register. Please try again.");
